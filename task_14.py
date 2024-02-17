9103c5b33b980a76c22b0875657c4b42322d554c
NessyMoonlight <159246841+NessyMoonlight@users.noreply.github.com>
2024-02-17 09:37:02 +0700

    fill end

---
+++ task_14.py
@@ -0,0 +1,11 @@
+a = float(input())
+a = a + 2
+a = a * 3
+a = a - 6
+a = a / 3
+a = a - 4
+print(a)
+
+#another version
+a = float(input())
+print(((((a+2)*3)-6)/3)-4)
---
+++ task_15.py
@@ -0,0 +1,9 @@
+distance = float(input())
+dums = distance / 2.54
+futs = dums / 12
+yards = futs / 3
+miles = yards / 1760
+print(yards,'ярдов')
+print(miles,'мили')
+print(futs,'футов')
+print(dums,'дюймов')
